e3dc3344c10a4d68711ed398d2aaf50dba346948
Aaron Koenig <neverinion@gmail.com>
2012-09-12 17:48:49 -0400

    clear

--- knowevo/incunabula/admin.py
+++
@@ -1,6 +0,0 @@
-from incunabula.models import Article, Reference, MasterArticle
-from django.contrib import admin
-
-admin.site.register(Article)
-admin.site.register(MasterArticle)
-admin.site.register(Reference)
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/aaronkoenig/Desktop/knowevo1/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
